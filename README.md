62b9c263d031cc3c3b701cb76567b04920701308
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2024-06-20 07:56:51 -0500

    Add q10

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # test_mern_render
 
 https://www.youtube.com/watch?v=-PHi1w1elxU&ab_channel=FaztCode
+
+
+https://www.youtube.com/watch?v=AF2gywytBzs&ab_channel=ProgramandoconDanCruise
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-      
-      <h1> MERN RENDER</h1>
-      <button 
-        onClick={ async () => {
-          const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/usuarios`);
-          const data = await res.json();
-          console.log(data);
-        }}
-      >Users</button>
+        <h1> MERN RENDER</h1>
+        <button
+          onClick={async () => {
+            const res = await fetch(
+              `${import.meta.env.VITE_BACKEND_URL}/usuarios`
+            );
+            const data = await res.json();
+            console.log(data);
+          }}
+        >
+          Users
+        </button>
+
+        <a href="https://www.q10.com/Colombia"  target="_blank">
+          Ir a Q10 Colombia
+        </a>
       </div>
-       
     </>
-  )
+  );
 }
 
-export default App
+export default App;
